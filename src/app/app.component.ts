033248c39762f7fe9ef43b6d81c9ae0c081d5d34
Deyanira <deyaniratrendez03@gmail.com>
2025-09-10 19:36:52 -0600

    Instalacion bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,33 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+/*  template:`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Hola Mundo</h1>
+</body>
+</html>
+  `,*/
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basesAngular';
+  title = 'Deyanira Contreras Fernandez';
+  duplicarNumero(valor:number):number{
+    return valor * 2;
+  }
+
+  pelicula={
+  titulo:'Mujercitas',
+fechaLanzamiento:new Date(),
+  director:'Greta Gerwig'
 }
+}
+
+
